df5f24cab3b72611c9038d3fd3bd2c6ed1aaf667
Mr.H <mike.hastie@gmail.com>
2018-10-31 21:07:23 -0400

    Restored settings
    Adding more comments

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,9 +31,9 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.15) {
+        if (r < 0.1) {
             fail("oops");
-        } else if (r < 0.25) {
+        } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
     }
